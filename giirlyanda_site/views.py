3fc7227306d2e70ae5249554db14f5d247dd3f86
valera122333 <71966469+valera122333@users.noreply.github.com>
2023-09-24 06:21:24 +1000

    Update views.py

--- giirlyanda_site/views.py
+++ giirlyanda_site/views.py
@@ -2,21 +2,16 @@ import os
 import cv2
 from PIL import Image
 import numpy as np
-
 import tensorflow as tf
 from django.conf import settings
 from django.template.response import TemplateResponse
 from django.utils.datastructures import MultiValueDictKeyError
-
 from django.core.files.storage import FileSystemStorage
-
 
 class CustomFileSystemStorage(FileSystemStorage):
     def get_available_name(self, name, max_length=None):
         self.delete(name)
         return name
-
-
 
 def index(request):
     message = ""
@@ -32,14 +27,9 @@ def index(request):
         imag=cv2.imread(path)
         img_from_ar = Image.fromarray(imag, 'RGB')
         resized_image = img_from_ar.resize((50, 50))
-
         test_image =np.expand_dims(resized_image, axis=0) 
-
-        # load model
         model = tf.keras.models.load_model(os.getcwd() + '/model.h5')
-
         result = model.predict(test_image) 
-        
 
         values = result[0]
         category_mapping = {
@@ -64,11 +54,6 @@ def index(request):
                 "ZVT86.103K-02": 18,
                     }
 
-        
-
-
-
-        
         valu = []
         for i in result.flat:
             valu.append(i)
@@ -100,22 +85,18 @@ def index(request):
             elif valu[index] >= 1000 and valu[index] <= 10000:
                 percentage = (valu[index]+10000)/200
                 detail_percentages.append({"name": category, "percentage": percentage})
- 
-     
+                
         detail_percentages.sort(key=lambda x: x["percentage"], reverse=True)
-
 
         max_percentage = 0
         max_category = None
-
-
+    
         for item in detail_percentages:
             percentage = item["percentage"]
             if percentage > max_percentage:
                 max_percentage = percentage
                 max_category = item["name"]
-
-
+                
             print("" + max_category)
 
         return TemplateResponse(
